dfb79d2a98b45d9637448f5d8cbe5daa5ba1009b
Joel Davila <joeldavila@Joels-MacBook-Pro.local>
2025-12-12 15:14:31 -0500

    added hook and created new component

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,18 +1,15 @@
+import { TodoListItem } from "./TodoListItem";
 
 
-const TodoList = () => {
+const TodoList = ({todoList}) => {
 
-    const todoList = [
-        { id: 1, title: "review resources" },
-        { id: 2, title: "take notes" },
-        { id: 3, title: "code out app" },
-    ]
+   
 
     return (
 
 
         <ul>
-            {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
+            {todoList.map(todo => < TodoListItem key={todo.id} todo={todo}/>)}
         </ul>
 
 
