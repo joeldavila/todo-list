4eed1c1846cb09c165142e7c0f342c1eb9f30aaf
Joel Davila <joeldavila@Joels-MacBook-Pro.local>
2025-12-23 22:54:48 -0500

    using useRef hook

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,13 +1,32 @@
+import { useRef } from "react";
+
+const TodoForm = ({onAddTodo}) => {
+  const inputRef = useRef();
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    
+    const todoTitle = event.target.todoTitle.value.trim();
+    if (todoTitle) {
+      onAddTodo(todoTitle);
+      event.target.reset();
+      inputRef.current.focus();
+    }
+  };
 
 
-const TodoForm = () => {
   return (
-    <form>
-      <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" />
-      <button type="submit" disabled>Add Todo</button>
-    </form>
-  );
+  <form onSubmit={handleAddTodo}>
+    <label htmlFor="todoTitle">Todo</label>
+    <input 
+      type="text" 
+      id="todoTitle" 
+      name="todoTitle"
+      ref={inputRef}
+      required
+    />
+    <button type="submit">Add Todo</button>
+  </form>
+);
 }
 
 export default TodoForm;
